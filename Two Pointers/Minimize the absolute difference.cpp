00265f52e95f86917803ba4180e2a1a261199784
Karan <86399274+b202001162@users.noreply.github.com>
2023-07-05 16:37:49 +0530

    Two Pointers

---
+++ Two Pointers/Minimize the absolute difference.cpp
@@ -0,0 +1,52 @@
+// Given three sorted arrays A, B and Cof not necessarily same sizes.
+
+// Calculate the minimum absolute difference between the maximum and minimum number from the triplet a, b, c such that a, b, c belongs arrays A, B, C respectively.
+// i.e. minimize | max(a,b,c) - min(a,b,c) |.
+
+// Example :
+
+// Input:
+
+// A : [ 1, 4, 5, 8, 10 ]
+// B : [ 6, 9, 15 ]
+// C : [ 2, 3, 6, 6 ]
+// Output:
+
+// 1
+// Explanation: We get the minimum difference for a=5, b=6, c=6 as | max(a,b,c) - min(a,b,c) | = |6-5| = 1.
+
+
+
+int Solution::solve(vector<int> &A, vector<int> &B, vector<int> &C) {
+    int i = 0, j = 0, k = 0, mini = INT_MAX;
+    while(i < A.size() && j < B.size() && k < C.size()) {
+        mini = min(mini, max(max(A[i],B[j]),C[k]) - min(min(A[i], B[j]), C[k]));
+        if(A[i] >= B[j]) {
+            if(A[i] >= C[k] ) {
+                if(B[j] >= C[k]) {
+                    k++;
+                    continue;
+                }
+                else {
+                    j++;
+                    continue;
+                }
+            }
+            else {
+                j++;
+                continue;
+            }
+        }
+        else {
+            if(A[i] >= C[k]) {
+                k++;
+                continue;
+            }
+            else {
+                i++;
+                continue;
+            }
+        }
+    }
+    return mini;
+}
